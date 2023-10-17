532017debf4a62f4d0eee6e5d00e3313dfc1bd1f
maitha1-nule <nuleahmed@gmail.com>
2023-10-18 02:57:21 +0300

    task 0

--- _printf.c
+++ _printf.c
@@ -11,6 +11,8 @@ int _printf(const char *format, ...)
 	unsigned int i, s_count, count = 0;
 	va_list args;
 
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 	va_start(args, format);
 
 	for (i = 0; format[i] != '\0'; i++)
