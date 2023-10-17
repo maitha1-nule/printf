f2152919b692d8833a15661f4c5bea2c3ddd5980
Buumaa <ajaoolubunmi552@gmail.com>
2023-10-17 16:24:01 -0700

    task1

--- main.h
+++ main.h
@@ -5,9 +5,14 @@
 #include <stdarg.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
 
+int _putchar(char c);
 int print(char c);
 int put(char *c);
 int _printf(const char *format, ...);
+int print_int(va_list args);
+int print_dec(va_list args);
 
 #endif
